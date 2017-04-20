c76335596f456d8cc267f8a6bcd378ada60cc1bb
Carter Green <cartergr@usc.edu>
2017-04-20 14:10:15 +0200

    Solutions for problems 25 and 29

--- 21_30/problem25.fs
+++ 21_30/problem25.fs
@@ -22,3 +22,14 @@ The 12th term, F12, is the first term to contain three digits.
 
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 *)
+// big integer Fibonacci sequence
+let fibonacciSeq = Seq.unfold (fun (a,b) -> Some( a+b, (b, a+b) ) ) (0I,1I)
+
+[<EntryPoint>]
+let main args =
+    fibonacciSeq
+    |> Seq.findIndex (fun n -> String.length (string n) = 1000)
+    // Account for F# 0 based indexing and generating the sequence starting with 0, 1 instead of 1, 1
+    |> (+) 2
+    |> printfn "The index of the first term in the Fibonacci sequence to contain 1000 digits is %A"
+    0
