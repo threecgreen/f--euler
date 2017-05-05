3a1a46bc4c090acf50f6e65a070f60c73750e841
Carter Green <cartergr@usc.edu>
2017-05-05 23:23:00 +0200

    Solution to problem 21

--- 21_30/problem21.fs
+++ 21_30/problem21.fs
@@ -8,3 +8,25 @@ For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 a
 
 Evaluate the sum of all the amicable numbers under 10000.
 *)
+let sumOfProperDivisors number =
+    match number with
+    | 0 | 1 -> 0
+    | _ when number > 1 ->
+        [1 .. (number / 2)]
+        |> List.filter (fun div -> number % div = 0)
+        |> List.reduce (+)
+
+let amicablePairs range =
+    let testRange = List.map (fun n -> (n, sumOfProperDivisors n)) range
+    List.map (fun n -> (n, sumOfProperDivisors n)) range
+    // Check if the sum of proper divisors equals another number in the range, the number equals a the sum of the proper divisors of another number and the number and its sum of
+    // proper divisors are not the same
+    |> List.filter ( fun (n, sum) -> List.exists (fun (n2, sum2) -> n2 = sum && n = sum2 && n <> sum) testRange )
+    |> List.map (fun (n, sum) -> n)
+
+[<EntryPoint>]
+let main args =
+    amicablePairs [3..9999]
+    |> List.reduce (+)
+    |> printfn "The sum of all amicable pairs under 10000 is %d."
+    0
